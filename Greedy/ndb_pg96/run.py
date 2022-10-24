b66d35ed0a6c77ccbecf479e33e7927cbaceec25
Dongmin Kim <38837579+carrtesy@users.noreply.github.com>
2022-10-24 15:30:04 +0000

    [20221025] ndb 96

--- Greedy/ndb_pg96/run.py
+++ Greedy/ndb_pg96/run.py
@@ -1,3 +1,10 @@
 N, M = map(int, input().split())
 print(N, M)
 
+lst = []
+MAX = 0
+for i in range(N):
+  row = list(map(int, input().split()))
+  _MAX = min(row)
+  MAX = _MAX if _MAX > MAX else MAX
+print(MAX)
